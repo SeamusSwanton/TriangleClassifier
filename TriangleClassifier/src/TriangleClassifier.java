3d315dace2a44f11e19c03fa4c4673b2dc815e4e
seamu <seamu@LAPTOP-HVE650HR>
2022-03-19 12:30:35 -0600

    e

--- TriangleClassifier/src/TriangleClassifier.java
+++ TriangleClassifier/src/TriangleClassifier.java
@@ -2,56 +2,57 @@
 public class TriangleClassifier {
 	
 
-private static boolean isValidTriangle = true;
-private static boolean isEquilateral = false;
 public static String message1 = "";
 public static String message2 = "";
 
 
 	public static boolean isValidTriangle(int i, int j, int k) {
 		if( (i + j + k != 180) || (i<1) || (j<1) || (k<1)) {
-			isValidTriangle = false;
+			return false;
 		}
-		return isValidTriangle;
+		else{
+			return true;
+		}
 			
 		
 	}
 
 	public static String classifyByAngle(int i, int j, int k) {
 	    if (i < 90 && j < 90 && k < 90) {
-	    	message1 = "acute";
+	    	return "acute";
 	    }
 	    else if (i == 90 || j == 90 || k == 90) {
-	    	message1 = "right";
+	    	return "right";
 	    }
 	    else {
-	    	message1 = "obtuse";
+	    	return "obtuse";
 	    }
-	    return message1;
 	}
 
 	public static String classifyBySide(int i, int j, int k) {
 		if ((i == j) && (i == k) && (j == k)) {
-			message2 = "equilateral";
+			return "equilateral";
 		}    
 	    
 		else if ((i == j) || (i == k) || (j == k)){
-			message2 = "isosceles";
+			return "isosceles";
 	    }   		
 	    else {
-	    	message2 = "scalene";
+	    	return "scalene";
 	    }
 		
-		return message2;
 	        		
 	}
 	
 	public static String getClassification(int i, int j, int k) {
-		if (isValidTriangle == false) {
+		if (isValidTriangle(i, j, k) == false){
 	        return "INVALID";
 		}
-	    else {    
-	        return (message1) + ' ' + (message2);
+		else if(classifyBySide(i, j, k) == "equilateral"){
+			return "equilateral";
+		}
+	    else {
+	        return (classifyByAngle(i, j, k)) + ' ' + (classifyBySide(i, j, k));
 	    }
 	}
 
